6b8743ca898cb5343919b28af384effc8c194f34
melissafg <melissa.flores.g@ucb.edu.bo>
2021-11-29 18:56:44 -0400

    falta

--- features/Step definition/Añadir_carrito.rb
+++ features/Step definition/Añadir_carrito.rb
@@ -1,4 +1,10 @@
 Then('Ahora mi carrito tiene el icono de {string}') do |string|
- cont = page.find('#header-aside > div > div.notification-wrap.header-cart-link-wrap.cart-wrap.menu-item-has-children > a > span > span')
- expect(cont).to have_content("1")
+  if string == "1"
+    cont = page.find('#header-aside > div > div.notification-wrap.header-cart-link-wrap.cart-wrap.menu-item-has-children > a > span > span')
+    expect(cont).to have_content("1")
+  elsif string == "2"
+    cont = page.find('#header-aside > div > div.notification-wrap.header-cart-link-wrap.cart-wrap.menu-item-has-children > a > span > span')
+    expect(cont).to have_content("2")
+  end
+
 end
--- features/Step definition/Ver_curso.rb
+++ features/Step definition/Ver_curso.rb
@@ -1,5 +1,10 @@
 When('hago click en la tarjeta {string}') do |string|
- page.find('#course-dir-list > ul > li:nth-child(3) > div > div.bb-card-course-details > h2 > a').click
+  if string =="como hacer milkshakes"
+    page.find('#course-dir-list > ul > li:nth-child(3) > div > div.bb-card-course-details > h2 > a').click
+  elsif string == "Curso completo de Photoshop desde Cero"
+    page.find('#course-dir-list > ul > li:nth-child(6) > div > div.bb-card-course-details > h2 > a').click
+  end
+
 end
 
 Then('Ahora estoy en la pagina {string}') do |string|
