564d5b66386cf0935b7c919c89cb1190553e2498
melissafg <melissa.flores.g@ucb.edu.bo>
2021-11-29 13:22:34 -0400

    No se pueden añadir 2 productos seguidos iguales

--- features/Step definition/Apuntarme_curso.rb
+++ features/Step definition/Apuntarme_curso.rb
@@ -9,3 +9,9 @@ end
 Then('Ahora estoy en la pagina de carrito {string}') do |string|
   expect(page).to have_content("Como hacer milkshakes")
 end
+Then('no puedo añadir otro curso igual') do 
+  click_button('add-to-cart')
+  expect(page).to have_content("No puedes añadir otro «Como hacer milkshakes» a tu carrito.")
+  cont = page.find('#header-aside > div > div.notification-wrap.header-cart-link-wrap.cart-wrap.menu-item-has-children > a > span > span')
+  expect(cont).to have_content("1")
+end
