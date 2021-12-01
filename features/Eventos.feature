d21f407f969b9bff50ff0ac1a7b9a4232d2709c3
luisMontanoRojas <lushio142356@gmail.com>
2021-12-01 19:45:42 -0400

    Added more scenarios.

--- features/Eventos.feature
+++ features/Eventos.feature
@@ -1,4 +1,7 @@
-Feature: I enter to eventos page
+Feature: Event page tests
+As a User
+I want to test the event page
+So I see the erros.
 
 @maximize
 Scenario: search wrong word
@@ -10,13 +13,13 @@ Scenario: search wrong word
 @maximize
 Scenario: Click on date      
   Given I visit "https://www.r-acad.com/eventos/"
-  And Press the "11/29/2021"
-  Then I see calendar message "noviembre 2021"
+  When Press the "11/29/2021"
+  Then I see calendar message "diciembre 2021"
 
 @maximize
-Scenario: Click on buscar
+Scenario: Click on buscar when I don't have anything to find
   Given I visit "https://www.r-acad.com/eventos/"
-  And Press the "Buscar Eventos" button
+  When Press the "Buscar Eventos" button
   Then I see message "No se ha encontrado ningún resultado."
 
 @maximize
@@ -24,7 +27,7 @@ Scenario: Click on filter by month
   Given I visit "https://www.r-acad.com/eventos/"
   When Press the "Lista" button
   And I click "Mes"
-  Then I see the message "Eventos en noviembre 2021"
+  Then I see the message "Eventos en diciembre 2021"
 
 @maximize
 Scenario: Click on filter by week
@@ -32,3 +35,44 @@ Scenario: Click on filter by week
   When Press the "Lista" button
   And I click "Semana"
   Then I see the message in week "Eventos para semana del noviembre 29, 2021"
+
+@maximize
+Scenario: Click on previous events
+  Given I visit "https://www.r-acad.com/eventos/"
+  When Press the "Eventos anteriores" button
+  Then I see the message "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+
+@maximize
+Scenario: Click on "Next Event" button a specific event
+  Given I visit "https://www.r-acad.com/eventos/"
+  When Press the "Eventos anteriores" button
+  And I click "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+  Then I see the title "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+
+@maximize
+Scenario: Click on "Next Event" button a specific event
+  Given I visit "https://www.r-acad.com/eventos/"
+  When Press the "Eventos anteriores" button
+  And I click "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+  And I click "Next Event"
+  Then I see the title "MASTERCLASS “Transformación Digital para tu Negocio”"
+
+@maximize
+Scenario: Click on "Previous Event" button a specific event
+  Given I visit "https://www.r-acad.com/eventos/"
+  When Press the "Eventos anteriores" button
+  And I click "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+  And I click "Next Event"
+  And I click "Previous Event"
+  Then I see the title "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+
+@maximize
+Scenario: Click on a specific date      
+  Given I visit "https://www.r-acad.com/eventos/"
+  When Press the "11/29/2021"
+  And I click "diciembre 2021"
+  And I click "2021"
+  And I click "2020"
+  And I click "Ene"
+  And I click "1"
+  Then I see the title "NOVIEMBRE 2020"
--- features/Step definition/Eventos.rb
+++ features/Step definition/Eventos.rb
@@ -2,61 +2,115 @@ Given('I visit {string}') do |string|
     visit string
   end
 
-  When(/^I search for "([^"]*)"$/) do |valueToSearch|
+When(/^I search for "([^"]*)"$/) do |valueToSearch|
   fill_in('tribe-bar-search',:with => valueToSearch).native.send_keys(:return)
-  end
+end
       
-  Then(/^I should see results "([^"]*)"$/) do |string|
-    message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[1]/h1')
-    if message.text != string
-      raise "result should be "+string
-    end
+Then(/^I should see results "([^"]*)"$/) do |string|
+  message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[1]/h1')
+  if message.text != string
+    raise "result should be "+string
   end
+end
 
 When(/^Press the "([^"]*)"$/) do |buttonName|
   find('input[name="tribe-bar-date"]').click
   end
 
-  Then(/^I see calendar message "([^"]*)"$/) do |string|
+Then(/^I see calendar message "([^"]*)"$/) do |string|
   message = find(:xpath, '/html/body/div[5]/div[1]/table/thead/tr[2]/th[2]')
   if message.text != string
     raise "result should be "+string
   end
 end
 
-When(/^Press the "([^"]*) button"$/) do |buttonName|
-find('input[name="submit-bar"]').click
+When('Press the {string} button') do |string|
+  if string =="Lista"
+    find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[2]/form/div[2]/div/button').click
+  elsif string ==  "Buscar Eventos"
+    find('input[name="submit-bar"]').click
+  end
+  if string == "Eventos anteriores"
+    find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[3]/div[3]/nav/ul/li/a').click
+  end
 end
 
 Then(/^I see message "([^"]*)"$/) do |string|
-message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[3]/div[1]/ul/li')
-if message.text != string
-  raise "result should be "+string
-end
-end
-
-When(/^Press the "([^"]*) button"$/) do |buttonName|
-find('input[id="tribe-bar-views-toggle"]').click
+  message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[3]/div[1]/ul/li')
+  if message.text != string
+    raise "result should be "+string
+  end
 end
 
 When(/^I click "([^"]*)"$/) do |buttonName|
-find('li[id="tribe-bar-views-option-month"]').click
+  if buttonName =="Mes"
+    find('li[id="tribe-bar-views-option-month"]').click
+  elsif buttonName=="Semana"
+    find('li[id="tribe-bar-views-option-week"]').click
+  end
+  if buttonName == "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+    find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div/h2/a').click
+  end
+  if buttonName == "Next Event"
+    find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[5]/ul/li[2]/a').click
+  end
+  if buttonName == "Previous Event"
+    find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[5]/ul/li[1]/a').click
+  end
+  if buttonName == "diciembre 2021"
+    find(:xpath, '/html/body/div[5]/div[1]/table/thead/tr[2]/th[2]').click
+  end
+  if buttonName == "2021"
+    find(:xpath, '/html/body/div[5]/div[2]/table/thead/tr[2]/th[2]').click
+  end
+  if buttonName == "2020"
+    find(:xpath, '/html/body/div[5]/div[3]/table/tbody/tr/td/span[2]').click
+  end
+  if buttonName == "Ene"
+    find(:xpath, '/html/body/div[5]/div[2]/table/tbody/tr/td/span[1]').click
+  end
+  if buttonName == "1"
+    find(:xpath, '/html/body/div[5]/div[1]/table/tbody/tr[1]/td[3]').click
+  end
 end
 
 Then(/^I see the message "([^"]*)"$/) do |string|
-message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[1]/h1')
-if message.text != string
-  raise "result should be "+string
-end
-end
-
-When(/^I click "([^"]*)"$/) do |buttonName|
-find('li[id="tribe-bar-views-option-week"]').click
+  if string=="Eventos en diciembre 2021"
+    message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[1]/h1')
+    if message.text != string
+      raise "result should be "+string
+    end
+  elsif string=="CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+    message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div/h2/a')
+    if message.text != string
+      raise "result should be "+string
+    end
+  end
 end
 
 Then(/^I see the message in week "([^"]*)"$/) do |string|
-message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[1]/h1')
-if message.text != string
-  raise "result should be "+string
+  message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[1]/h1')
+  if message.text != string
+    raise "result should be "+string
+  end
 end
+
+Then('I see the title {string}') do |string|
+  if string == "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
+    message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[2]/div[2]/div[1]/h1')
+    if message.text != string
+      raise "result should be "+string
+    end
+  elsif string == "MASTERCLASS “Transformación Digital para tu Negocio”"
+    message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[2]/div[2]/div[1]/h1')
+    if message.text != string
+      raise "result should be "+string
+    end
+  end
+  if string == "NOVIEMBRE 2020"
+    message = find(:xpath, '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[3]/div[2]/h2[1]/span')
+    if message.text != string
+      raise "result should be "+string
+    end
+  end
 end
