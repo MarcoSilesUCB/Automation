337bf40d0306fc1c8cacfcb75ab9c0c073aa7b87
melissafg <melissa.flores.g@ucb.edu.bo>
2021-12-02 18:44:59 -0400

    arreglos del primer merge

--- features/Step definition/Filtrar_profesor.rb
+++ features/Step definition/Filtrar_profesor.rb
@@ -6,12 +6,19 @@ end
 
 
 Then('las tarjetas de cursos ahora solo muestran los cursos de Sebastian.')do 
-for a in 1..8 do
-  curso =find(:css, "#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid > li:nth-child(#{a})> div > div.bb-card-course-details > div.bb-course-meta > strong > a")
-  if  curso.text != "Sebastián"
-    raise "no se filtro bien"
+
+list = Array.new 
+list = find('#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid').all('li.bb-course-item-wrap') 
+
+puts(list.size)
+  for a in 1..list.size  do
+    
+    curso =find(:css, "#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid > li:nth-child(#{a})> div > div.bb-card-course-details > div.bb-course-meta > strong > a")
+    puts(curso)
+    if  curso.text != "Sebastián"
+      raise "no se filtro bien"
+    end
+    
   end
-   
- end
 
 end
