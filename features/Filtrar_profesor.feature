f9ecfff9c3d31e4ee995b17f6e49c3cce1aa951e
melissafg <melissa.flores.g@ucb.edu.bo>
2021-12-03 11:41:34 -0400

    ultimo filtro arreglado para 2 instructores

--- features/Filtrar_profesor.feature
+++ features/Filtrar_profesor.feature
@@ -1,11 +1,17 @@
 Feature: test filtrar por profesor
 As Interesado en cursos en linea
-I want Poder filtrar los cursos solo por el instructor Sebastian
-So podre encontrar los curso del profesor Sebastian mas rapido
+I want Poder filtrar los cursos solo por el instructor 
+So podre encontrar los cursorelacionados al instructor mas rapido
 
-Scenario: Filtrar curso por instructor     
+Scenario: Filtrar curso por instructor Sebastián    
   Given estoy en la pagina principal
   And hago click en "cursos"
   When Selecciono en el ultimo filtro "Sebastian"
-  Then las tarjetas de cursos ahora solo muestran los cursos de Sebastian.
+  Then las tarjetas de cursos ahora solo muestran los cursos de "Sebastian".
+ 
+ Scenario: Filtrar curso por instructor RottweilerAcademy    
+  Given estoy en la pagina principal
+  And hago click en "cursos"
+  When Selecciono en el ultimo filtro "rottweilergroupbolivia"
+  Then las tarjetas de cursos ahora solo muestran los cursos de "rottweilergroupbolivia".
  
--- features/Step definition/Filtrar_profesor.rb
+++ features/Step definition/Filtrar_profesor.rb
@@ -1,24 +1,40 @@
 
 When('Selecciono en el ultimo filtro {string}') do |string|
-  page.find('#sfwd_instructors-order-by').find(:xpath, 'option[2]').click
+
+  if string =="Sebastian"
+    page.find('#sfwd_instructors-order-by').find(:xpath, 'option[2]').click
+  elsif string == "rottweilergroupbolivia"
+    page.find('#sfwd_instructors-order-by').find(:xpath, 'option[3]').click
+  end
+  
 
 end
 
 
-Then('las tarjetas de cursos ahora solo muestran los cursos de Sebastian.')do 
+Then('las tarjetas de cursos ahora solo muestran los cursos de {string}.')do |string|
+  if string == "Sebastian"
+    list = Array.new 
+    list = find('#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid').all('li.bb-course-item-wrap') 
+      for a in 1..list.size  do
+        
+        curso =find(:css, "#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid > li:nth-child(#{a})> div > div.bb-card-course-details > div.bb-course-meta > strong > a")
 
-list = Array.new 
-list = find('#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid').all('li.bb-course-item-wrap') 
+        if  curso.text != "Sebastián"
+          raise "no se filtro bien"
+        end
+        
+      end
+  elsif string == "rottweilergroupbolivia"
+    list = Array.new 
+    list = find('#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid').all('li.bb-course-item-wrap') 
+      for a in 1..list.size  do
+        
+        curso =find(:css, "#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid > li:nth-child(#{a})> div > div.bb-card-course-details > div.bb-course-meta > strong > a")
 
-puts(list.size)
-  for a in 1..list.size  do
-    
-    curso =find(:css, "#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid > li:nth-child(#{a})> div > div.bb-card-course-details > div.bb-course-meta > strong > a")
-    puts(curso)
-    if  curso.text != "Sebastián"
-      raise "no se filtro bien"
-    end
-    
+        if  curso.text != "rottweilergroupbolivia"
+          raise "no se filtro bien"
+        end
+        
+      end
   end
-
 end
