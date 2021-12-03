1b70b3040ae04ee50125e8f3327743eae1adbb53
melissafg <melissa.flores.g@ucb.edu.bo>
2021-12-02 22:43:03 -0400

    comprobando forms

--- features/Step definition/Finalizar_compra_login.rb
+++ features/Step definition/Finalizar_compra_login.rb
@@ -1,5 +1,14 @@
 Then('Ahora puedo llenar los detalles de facturación.') do
  titulo = "#customer_details > div.col-1 > div > h3"
- expect(page).to have_content("Finalizar compra")
+  expect(page).to have_content("Finalizar compra")
   expect( find(:css, titulo)).to have_content("DETALLES DE FACTURACIÓN")
+  campo_nombre = "#billing_first_name_field > label"
+  find(campo_nombre)=="Nombre"
+  campo_apellido = "#billing_last_name_field > label"
+  find(campo_apellido)=="Apellidos"
+  campo_direccion = "#billing_address_1_field > label"
+  find(campo_direccion) == "Dirección de la calle"
+  campo_ciudad = "#billing_city_field > label"
+  find(campo_ciudad) == "Localidad / Ciudad "
 end
+#post-29 > div > div > form > table > tbody > tr:nth-child(2) > td.product-remove
