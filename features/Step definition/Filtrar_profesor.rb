e2efe87a388dc85f56e5e0a3323c190026ba3680
luisMontanoRojas <lushio142356@gmail.com>
2021-12-03 19:27:03 -0400

    Conflictos resueltos

--- features/Step definition/Filtrar_profesor.rb
+++ features/Step definition/Filtrar_profesor.rb
@@ -31,29 +31,10 @@ Then('las tarjetas de cursos ahora solo muestran los cursos de {string}.')do |st
         
         curso =find(:css, "#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid > li:nth-child(#{a})> div > div.bb-card-course-details > div.bb-course-meta > strong > a")
 
-<<<<<<< HEAD
-Then('las tarjetas de cursos ahora solo muestran los cursos de Sebastian.')do 
-
-list = Array.new 
-list = find('#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid').all('li.bb-course-item-wrap') 
-
-puts(list.size)
-  for a in 1..list.size  do
-    
-    curso =find(:css, "#course-dir-list > ul.bb-card-list.bb-course-items.grid-view.bb-grid > li:nth-child(#{a})> div > div.bb-card-course-details > div.bb-course-meta > strong > a")
-    puts(curso)
-    if  curso.text != "Sebastián"
-      raise "no se filtro bien"
-    end
-    
-  end
-
-=======
         if  curso.text != "rottweilergroupbolivia"
           raise "no se filtro bien"
         end
         
       end
   end
->>>>>>> eac8d5d5f34e01457e4d840e342bc203a5f75003
 end
